ea7d8ae5a5e81ee6b1a32cef6f201a7d5325c9ab
Xavi Garcia <antiwindows17@gmail.com>
2017-04-25 20:21:10 +0200

    gpio test

--- RaspberryBackground/StartupTask.cs
+++ RaspberryBackground/StartupTask.cs
@@ -12,16 +12,39 @@ using Newtonsoft.Json.Linq;
 
 namespace RaspberryBackground {
     public sealed class StartupTask : IBackgroundTask {
-
         private CloudClient client;
         private Random rng;
         private Timer timer;
         private int delay = 4000;
         private const int DeviceId = 2;
+        private const int switch_pin = 2;
+        private const int led_pin = 21;
 
         private BackgroundTaskDeferral deferral;
         private GpioController GPIO;
-        private GpioPin output;
+        private GpioPin switchPin;
+        private GpioPin ledPin;
+
+        private void initGPIO() {
+            GPIO = GpioController.GetDefault();
+            if (GPIO == null) {
+                Debug.WriteLine("Cannot access Pin controller");
+                return;
+            }
+            ledPin = GPIO.OpenPin(led_pin);
+            ledPin.Write(GpioPinValue.Low);
+            ledPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            switchPin = GPIO.OpenPin(switch_pin);
+            switchPin.SetDriveMode(GpioPinDriveMode.Input);
+            switchPin.DebounceTimeout = TimeSpan.FromMilliseconds(50);
+            //pin.ValueChanged += (sender, args) => {
+            //    Debug.WriteLine(pin.Read());
+            //};
+
+            //var value = pin.Read();
+            //Debug.WriteLine(value);
+        }
 
         public async void Run(IBackgroundTaskInstance taskInstance) {
             // 
@@ -35,64 +58,49 @@ namespace RaspberryBackground {
             rng = new Random();
             initGPIO();
             client = new CloudClient {BaseUri = new Uri("https://cloudtfg.azurewebsites.net")};
-            
+
             deferral = taskInstance.GetDeferral();
             taskInstance.Canceled += TaskInstanceOnCanceled;
             try {
                 await client.GetAccessToken();
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 Debug.WriteLine(e);
             }
-            
+
             timer = new Timer(Callback, null, 0, delay);
-        }
-
-        private void initGPIO() {
-            GPIO = GpioController.GetDefault();
-            if (GPIO == null) {
-                Debug.WriteLine("Cannot access Pin controller");
-                return;
-            }
-
-            output = GPIO.OpenPin(5);
-            output.Write(GpioPinValue.High);
-            output.SetDriveMode(GpioPinDriveMode.Output);
-
-            var value = output.Read();
-            Debug.WriteLine(value);
-            
-        }
+        }   
 
         private void TaskInstanceOnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason) {
             if (reason == BackgroundTaskCancellationReason.Terminating) {
                 timer.Dispose();
-                output.Dispose();
+                switchPin.Dispose();
                 deferral?.Complete();
             }
         }
 
         private async void Callback(object state) {
             try {
-                var device = (Devices)client.GetDevices(DeviceId);
+                var switch_read = switchPin.Read();
+                Debug.WriteLine(switch_read);
+                ledPin.Write(switch_read);
+                var device = (Devices) client.GetDevices(DeviceId);
 
                 if (device.DeviceEnabled) {
                     //TODO read from output pin
-                    var result = await client.HttpPostData(new HistoricData(DeviceId, rng.Next(20, 25).ToString(), 1));
+                    var active_state = switch_read == GpioPinValue.Low ? "Active" : "Inactive";
+                    var result = await client.HttpPostData(new HistoricData(DeviceId, active_state, 5)); //rng.Next(20, 25).ToString() type 1 type 5 == activation state
                     Debug.WriteLine(result.Body.ToString());
 
                     if (result.Response.StatusCode == HttpStatusCode.Created) {
                         var data = (JObject) result.Body;
-                        var interval = (int)data["Interval"];
+                        var interval = (int) data["Interval"];
                         if (interval != delay) {
                             delay = interval;
                             timer.Change(0, delay);
                         }
                     }
                 }
-
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 Debug.WriteLine(e.Message);
             }
         }
